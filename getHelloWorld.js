727b4430b1a9b4309a1d9d3baafe0a6378632adf
尉一飞 <weiyifei@qingting.fm>
2020-07-09 14:56:51 +0800

    init test project

--- getHelloWorld.js
+++ getHelloWorld.js
@@ -1,26 +1,53 @@
-const Router = require('@koa/router')
 const Koa = require('koa')
-const hello = require('./test')
-const { route } = require('./test')
-const bodyParser = require('koa-bodyparser')
+const Router = require('@koa/router')
+const body = require('koa-bodyparser');
 
-const router = new Router()
-const app = new Koa()
+const mongoose = require('mongoose')
 
+const app = new Koa();
+const router = new Router();
 
+mongoose.connect('mongodb://localhost/test');
 
-app.use(bodyParser())
+const schema = mongoose.Schema({
+  room_id: {
+    type: String,
+  },
+  start_time: {
+    type: String,
+  },
+  end_time: {
+    type: String,
+  },
+  playback_url: {
+    type: String,
+  },
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+});
 
-router.post("/test", async (ctx, next) => {
-  console.log(ctx.request.body)
-  const name = ctx.request.body.name
-  console.log(name)
-  if (!name) {
-    throw new Error('i am wrong')
-  }
-  console.log('zs')
+const PlaybackRecord = mongoose.model('playback_records', schema);
+
+const playbackRecord = new PlaybackRecord({
+  room_id: "12345",
+  start_time: "1997-03-13 11:22:33",
+  end_time: "1997-03-13 11:22:33",
+  playback_url: "www.baidu.com"
+});
+
+app.use(body())
+
+router.post('/find', async (ctx, next) => {
+  // const docs = await PlaybackRecord.find({room_id: "12345"}).skip(1).limit(5);
+  const params = ctx.request.body
+  ctx.state = 200
+  ctx.body = params
 })
+  
 
-app.use(router.routes()).use(router.allowedMethods())
 
-app.listen(8888)
+app.use(router.routes(), router.allowedMethods());
+app.listen(8888);
--- test.js
+++ test.js
@@ -1,37 +1,4 @@
-const Router = require('@koa/router')
-
-let router = new Router()
-
-router.use(async (ctx, next) => {
-    console.log('router prev use')
-    await next()
-})
-
-router.get("/test", two, one, three)
-
-router.use(async (ctx, next) => {
-    console.log('router next use')
-    await next()
-})
-
-
-
-async function one(ctx, next) {
-  console.log('第一个')
-  console.log(ctx.name)
-  await next()
-}
-
-async function two(ctx, next) {
-  console.log('第二个')
-  await next()
-}
-
-async function three(ctx, next) {
-  console.log('第三个')
-  await next()
-}
-
-
-
-module.exports = router
+var str = 'zegotest-445349714-development_a74b6700681511eabd8a891cfe5ff1f9_a74d62d0-6815-11ea-bd8a-891cfe5ff1f9';
+var pattern = /[a-zA-Z0-9-]*/g;
+const len = str.match(pattern).length;
+console.log(str.match(pattern)[len-4]);
